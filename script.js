16c1053e9c134cbd8422a282918ead1590f988ee
AlessandraDerkas <111014122+AlessandraDerkas@users.noreply.github.com>
2024-11-07 08:53:57 -0300

    1234567

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 //Exemplo de itens no carrinho
-const itens = [{nome:"Produto A", quantidade:2, preco:30.00},
-    {nome:"Produto B", quantidade:1, preco:50.00},
-    
+const itens = [
+    {nome:"Produto A", quantidade:2, preco:30.00},
+    {nome:"Produto B", quantidade:1, preco:50.00},   
 ];
-functionatualizarCarrinho(){
+function atualizarCarrinho() {
     const body = document.getElementById('itensCarrinho');
     let total = 0;
-    itens.forEach(item => {const tr = document.createElement('tr');
+
+    itens.forEach(item => {
+        const tr = document.createElement('tr');
         const totalItem = item.quantidade*item.preco;
         total += totalItem;
-        tr.innerHTML = `${item.nome}${item.quantidade} R$ ${item.preco.toFixed(2)} R$ ${totalItem.toFixed(2)}`;
+
+        tr.innerHTML = `
+        <td>${item.nome}</td>
+        <td>${item.quantidade}</td>
+        <td>R$ ${item.preco.toFixed(2)}</td>
+        <td>R$ ${totalItem.toFixed(2)}</td>
+        `;
         body.appendChild(tr);
-    })
+    });
+
+    document.getElementById('totalCarrinho').innerText = `Total: R$ ${total.toFixed(2)}`;
 }
+
+function finalizarCompra() {
+    alert("Compra finaliza!");
+}
+
+// Atualiza o carrinho ao carregar a página window.onload = atualizarCarrinho;
